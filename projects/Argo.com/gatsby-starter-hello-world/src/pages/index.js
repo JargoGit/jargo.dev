af9b32e0d05d82817a08abb27451adcdee972036
Jeff Argonish <Jeffargonish@gmail.com>
2025-08-05 05:35:22 -0400

    update

--- projects/Argo.com/gatsby-starter-hello-world/src/pages/index.js
+++ projects/Argo.com/gatsby-starter-hello-world/src/pages/index.js
@@ -1,5 +1,5 @@
-import * as React from "react"
+import * as React from "react";
 
-export default function Home() {
-  return <div>Hello world!</div>
+export function Home() {
+  return <div>Hello world!</div>;
 }
